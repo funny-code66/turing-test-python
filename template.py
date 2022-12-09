6e261527c9614421145300a2330148c555fe2ac2
bright113gene <bright113gene@gmail.com>
2022-12-09 11:27:04 -0800

    add template.py

---
+++ template.py
@@ -0,0 +1,8 @@
+def findJudge(N, trust) -> int:
+res = None
+return result
+
+if __name__ == "__main__":
+  N = int(input())
+  trust = [[int(val) for val in pair.split()] for pair in input().strip().split(',')]
+  print(findJudge(N, trust))
